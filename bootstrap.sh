3724161fddfb956d13a66f3cb95236e7f824bf40
vpavlu <vpavlu@gmail.com>
2009-12-17 01:02:57 +0100

    git-based bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
-
-
-function dl {
-  fn="$1"
-  dir=`dirname "$fn"`
-  BASEURL=http://vpavlu.plain.at/share/base/files
-
-  mkdir -p "$dir"
-  wget $BASEURL/$fn -P $HOME/$dir/
-}
-
-# custom configs and tools
-dl bin/luks
-dl bin/t.sh
-dl bin/sync-files
-dl .profile
-dl .bash_aliases
-dl .vimrc
-
-
-# custom configs and tools
-#sudo aptitude installg git-core
-#git clone ssh+git://plain.at/home/vpavlu/base
-#mv base/* $HOME/
-#rm -rf base
+sudo aptitude install git-core
+git clone --depth 1 ssh://plain.at/home/vpavlu/base
+rm -rf base/.git
+cp -rfv base/home/.* $HOME/
+cp -rfv base/home/* $HOME/
+rm -rf base
 
 # utils
 #sudo aptitude install
