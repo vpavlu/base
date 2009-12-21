8235ba6a6fc81d2c50db9230023611344cffc3d7
vpavlu <vpavlu@gmail.com>
2009-12-21 17:00:54 +0100

    moved aptitude into separate file called from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,90 +12,13 @@ find . -type f -name .\* \
   -exec cp -v \{\} $HOME/\{\} \;
 popd
 
-sudo aptitude -y install \
-  vim
+# -- install vim
+sudo aptitude -y install vim
 
-
+# -- install rest
 bash base/setup-luks
 bash base/drei/setup-drei.at
+bash base/setup-packages
 
-
-#source base/setup-fluxbox
-echo sudo aptitude -y install \
-  xorg \
-  fluxbox \
-  rungetty \
-  thunar \
-  vim-gtk
-
-# FIXME rungetty auto-login
-# FIXME autostart of x server
-
-# utils
-echo sudo aptitude -y install \
-  unison \
-  apt-file \
-  acpi \
-  pwgen \
-  sshfs \
-  openssh-client \
-  ntfsprogs \
-  dosfstools \
-  bash-completion \
-  man \
-  locate \
-  powertop \
-  unzip \
-  wget
-
-# coding
-echo sudo aptitude -y install \
-  gcc \
-  g++ \
-  make
-
-# coding docs
-echo sudo aptitude -y install \
-  gcc-doc \
-  manpages \
-  manpages-dev \
-  glibc-doc-reference \
-  manpages-posix \
-  manpages-posix-dev
-
-# graphics
-echo sudo aptitude -y install \
-  gimp \
-  imagemagick \
-  vlc \
-  gpicview
-
-# latex
-echo sudo aptitude -y install \
-  evince \
-  texlive-latex-base \
-  texlive-latex-recommended \
-  texlive-latex-extra
-
-# latex docs
-#sudo aptitude -y install \
-#  texlive-latex-base-doc \
-#  texlive-latex-recommended-doc \
-#  texlive-latex-extra-doc
-
-# office
-echo sudo aptitude -y install \
-  openoffice.org-writer \
-  openoffice.org-calc
-
-# internet
-echo sudo aptitude -y install \
-  firefox \
-  pidgin \
-  sun-java6-plugin
-
-# lamp
-echo sudo aptitude -y install \
-  libapache2-mod-php5 php5-mysql mysql-server mysql-client
-
+# -- cleanup
 rm -rf base
